6154cc445257b0d144905c2d20274931a0caaa66
Salman Farshi <sfarshi222@gmail.com>
2026-06-24 12:28:29 +0600

    initial setup for finance

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Button} from "@heroui/react";
+
+function App() {
+  return (
+
+   <>
+    <div className='text-4xl bg-red-400 text-center py-3 font-bold text-white'>App</div>
+
+       <Button variant="secondary">Secondary</Button>
+   </>
+  )
+}
+
+export default App
